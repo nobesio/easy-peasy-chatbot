44adef34b8a55bc99383a5744c11a77046948c78
Nicolás Martín Obesio <nobesio@users.noreply.github.com>
2016-12-18 13:29:41 -0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -3,8 +3,6 @@ import xml.etree.ElementTree as ET
 
 knowledge = []
 
-#Cargamos los conceptos desde el archivo xml
-
 tree = ET.parse('knowledge.xml')
 concepts = tree.getroot()
 for concept in concepts:
@@ -12,19 +10,14 @@ for concept in concepts:
     for stimulus in concept.find("stimuli"):
         stimuli.append(stimulus.text)
     response = concept.find("response").text
-    knowledge.append( {"stimuli": stimuli, "response": response} ) # dic
-
+    knowledge.append( {"stimuli": stimuli, "response": response} )
 
 def concept_similarity(concept, user_msg):
     return max([nlp.cosine_sim(user_msg, stimulus) for stimulus in concept["stimuli"]])
 
-
 def get_best_response(knowledge, user_msg):
     return max(knowledge, key=lambda c: concept_similarity(c, user_msg))["response"]
-
 
 while True:
     user_msg = input("> ")
     print(get_best_response(knowledge, user_msg))
-
-
